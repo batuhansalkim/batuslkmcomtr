d097ebcb261e971f48b9155fe8a3685c22964153
batuhan <batusalkim11@gmail.com>
2024-07-15 22:54:59 +0300

    navbar bitti hero da az kaldı

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,54 @@
 import Image from "next/image";
 import camp_svg from "../public/camp.svg";
+import star_svg from "../public/star.svg";
+import Button from "./Button";
+import play_svg from "../public/play.svg";
+import close_svg from "../public/close.svg";
 
 function Hero(){
     return(
-        <div className="max-container padding-container border-2 border-red-500 flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+        <div className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
             <div className="hero-map"/>
             
             <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
                 <Image src={camp_svg} alt="camp" width={50} height={50} className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"/>
+                <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+                <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app</p>
+                <div className="my-11 flex flex-wrap gap-5">
+                    <div className="flex items-center gap-2">
+                        {Array(5).fill(1).map((_,index)=>(
+                            <Image
+                                src={star_svg}
+                                key={index}
+                                alt="star"
+                                width={24}
+                                height={24}
+                            />
+                        ))}
+                        <p className="bold-16 lg:bold-20 text-blue-70">
+                            198k
+                            <span className="regular-16 lg:regular-20 ml-1 underline">Excellent Reviews</span>
+                        </p>
+                    </div>
+                    <div className="flex flex-col w-full gap-3 sm:flex-row">
+                        <Button type="button" title="Download App" variant="btn_green"/>
+                        <Button type="button" title="How we work?" icon={play_svg} variant="btn_white_text"/>
+                    </div>
+                </div> 
+
+                <div className="relative flex flex-1 items-start ">
+                    <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+                        <div className="flex flex-col">
+                            <div className="flexBetween">
+                                <p className="regular-16 text-gray-20">Location</p>
+                                <Image src={close_svg} alt="close" width={24} height={24}/>
+                            </div>
+                            <div className="flexBetween">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
